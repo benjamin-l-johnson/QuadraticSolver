5230fecdf9f3f4a272e7fb5acba3deb2eb8ef90d
Ben <benjamin-l-johnson@github.com>
2014-03-22 16:12:37 -0400

    added some switch options for user input

--- src/quadratic.c
+++ src/quadratic.c
@@ -1,24 +1,50 @@
 #include "quadratic.h"
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {	
 	//printf("%d\n", argc );
 	char * input;
 	size_t size = 0;
 	double a,b,c,x1,x2, linans;
-	char* strA,strB,strC,check;
-	strA=NULL;
-	strB=NULL;
-	strC=NULL;
-	check=NULL;
+
+	char* strA,*strB,*strC,*check;
+
 	switch(argc)
 	{
 		//they entered no command line args
 		case 1:
-			printf("Enter Variables a b and c in a space seprated string");
-			getline(&input,size,stdin);
+			printf("Enter Variables a b and c in a space seprated string\n");
+			getline(&input,&size,stdin);
+			if(sscanf(input, "%lf %lf %lf\n",&a,&b,&c) != 3)
+			{
+				fprintf(stderr, "incorrect args \n");
+				exit(0);
+			}
+			printf("%s\n",input);
+			break;
+		//they entered one large string 
+		case 2:
+			if(sscanf(argv[1], "%lf %lf %lf\n",&a,&b,&c) != 3)
+			{
+				fprintf(stderr, "incorrect args \n");
+				exit(0);
+			}
+
+			break;
+
+		//they entered three command line args
+		case 4:
+			a = string_to_double(argv[1]);
+			b = string_to_double(argv[2]);
+			c = string_to_double(argv[3]);
+			break;
+
+		default:
+			fprintf(stderr, "command unknown");
+			exit(0);
+			break;
 	}
-	printf("a=%.8lf, b=%.8lf, c=%.8lf\n",a,b,c );
+
 
 	if (a==0)
 	{
